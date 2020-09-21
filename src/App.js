47194a054a7b8e7029fe23b189930c7c1b034d36
bhlee0410 <bhlee.nurim@gmail.com>
2020-09-21 22:20:56 +0900

    create react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+ render () {
+  return (
+    <div className="gray-background">
+       <img src={logo} late="logo" />
+       <h2> 개발 Management System </h2>
+    </div>
+  );
+
+ }
+}
+
+
+export default App;
